1c1028787b9650d7d6a34f6b99f6d3edfd5727c2
Prakhar Tiwari <39404339+prakhartiwari184@users.noreply.github.com>
2022-11-04 22:24:29 +0530

    Add files via upload

---
+++ forLoop.js
@@ -0,0 +1,15 @@
+function validate(flag){
+   for(let i=0;i<10;i++){
+       console.log("value of i is : ",i);
+   }
+   console.log("end of for");
+}
+    
+    validate(3);
+    //console.log("true"=="true");
+    var j=0;
+while(j!=20){
+    console.log(j);
+    j++;
+}
+    // for(initialisation;condition;increment)
---
+++ ifElse.js
@@ -0,0 +1,14 @@
+function validate(flag){
+if(flag[0]==2){
+    console.log("validated");
+}
+else if(flag[2]==1){
+    console.log("not validated");
+}
+else{
+    console.log("last object");
+}
+}
+
+validate([1,2,4]);
+//console.log("true"=="true");
