1a5a07e7c436f3a9f053f235a78b2c4e29b4da0f
Prakhar Tiwari <39404339+prakhartiwari184@users.noreply.github.com>
2022-11-21 22:05:52 +0530

    Add files via upload

--- arrow.js
+++ arrow.js
@@ -1,21 +1,103 @@
-const contents = document.querySelectorAll('.content')
-const listItems = document.querySelectorAll('nav ul li')
-
-listItems.forEach((item, idx) => {
-    item.addEventListener('click', () => {
-        hideAllContents()
-        hideAllItems()
-
-        item.classList.add('active')
-        contents[idx].classList.add('show')
-    })
-})
-
-function hideAllContents() {
-    contents.forEach(content => content.classList.remove('show'))
+/*let x = function (x,y){ // first class function 
+    return x*y;
 }
 
+let y = (x,y)=>x*y; // arrow function
 
-function hideAllItems() {
-    listItems.forEach(item => item.classList.remove('active'))
+console.log(y(3,2));
+
+// 1. when function has multiple lines 
+let myfunction = (arg1,arg2,...argN)=>{
+    // code lines 
+    statements(s); // code lines of the function 
 }
+// 2. when function has single return statement 
+
+let myFunction = (arg1,arg2,...argN)=>expression 
+
+
+//1. no param or args arrow func
+let abc=()=>console.log("testing no param or args function");
+//abc();
+
+//2. arrow function with single param or argument
+
+let xyz=x=>console.log(x);
+
+//xyz("func returning single param passed");
+
+
+
+ //3. arrow function as an expression 
+
+let age=5;
+
+let welcome= (age <18)?()=>console.log("baby"):()=>console.log("Adult")
+
+
+
+
+//if(age>18){}else{}
+
+//(age>18)?console.log("Hi"):console.log("bye")
+
+
+
+// 4. Multiline arrow function 
+
+let sum = (a,b) => {
+    let result=a+b;
+    return result;
+}
+let result1=sum(2,4);
+console.log(result1);
+
+
+
+// Inside a regular func this keyword refers to the function where it is called 
+
+function person (){
+    this.name ='jack';
+    this.age = 25;
+    this.sayName=function (){
+        console.log(this.age);
+        function innerFunction(){ // regular func cannot refer to parent scopes
+            console.log(this.age)
+            console.log(this);
+        }
+        innerFunction();
+    }
+}
+function person1 (){
+    this.name ='jack';
+    this.age = 25;
+    this.sayName=function (){
+        console.log(this.age);
+        let innerFunction=()=>console.log(this.age);// arrow function refers to parent scope so can use attributes of parent 
+        innerFunction();
+    }
+}
+let x= new person();
+let y= new person1();
+
+//x.sayName();
+y.sayName();
+
+
+let x= function(){
+    console.log(arguments[1]);
+}
+
+//x(4,5,6);
+
+let y=(...v)=>console.log(v[1]); // spread opertor which denotes as three dots ...
+y(1,2,3);
+*/
+let person={
+    name:"Prakhar",
+    age:16,
+    sayName:function (){
+        console.log(this);
+    }
+};
+person.sayName();
